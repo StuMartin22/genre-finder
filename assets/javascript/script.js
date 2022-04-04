111e0998c263990e280487a11556ad84dcf300e6
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-04 13:49:30 -0500

    Co-authored-by: EricPhilipps <EricPhilipps@users.noreply.github.com> Co-authored-by: GLundy99 <GLundy99@users.noreply.github.com>

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+var urlData = document.location.search;
+var userInput = $('#userInput').val();
+var genreSearch; //= url split
+
+var apiUrl; //= url + genreSearch/genreId + albums
+
+// when search is added on results html
+var searchBtn = $('#search-btn');
+
+searchBtn.on("click", function(e) {
+    e.preventDefault();
+    //var artist = ;
+    //var album = ;
+
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        }
+        .then (function (data){
+            displayGenre();
+            
+        })
+    )
+    })
+
+function displayGenre() {
+    // pull from api text and images to display in html
+    
+}
