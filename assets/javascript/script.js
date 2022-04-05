5c80e56c824f1ce204315dd0b5f338b2bbcaf16e
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-05 14:01:58 -0500

    userinput now being put into URL and logged properly. Changed Id class on HTML elements to properly pull userInput from form. changed values of dropdown form values.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 var urlData = document.location.search;
-// var userInput = $('#userInput').val();
-var genreSearch; //= url split
+var userInput = $('.userInput');
 
-var apiUrl; //= url + genreSearch/genreId + albums
+// var genreSearch; //= url split
+
+// var apiUrl; //= url + genreSearch/genreId + albums
+
+var apiUrl = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&limit=10&api_key=38f325c730f4218c2247c79ff7fd0a85&format=json"
 
 // initializes the form
     $('select').formSelect();
@@ -14,28 +17,34 @@ var apiUrl; //= url + genreSearch/genreId + albums
 // when search is added on results html
 var searchBtn = $('#search-btn');
 
-searchBtn.on("click", function(e) {
-    e.preventDefault();
+
+
+searchBtn.on("click", function(event) {
+    console.log(userInput.val());
+    event.preventDefault(event);
+    var newURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=" + userInput.val() + "&limit=10&api_key=38f325c730f4218c2247c79ff7fd0a85&format=json";
+    // console.log(newURL);
+});
+});
+
+
+
     //var artist = ;
     //var album = ;
 
-    fetch(apiUrl)
-        .then(function(response) {
-            return response.json();
-        }
-        .then (function (data){
-            displayGenre();
+    // fetch(apiUrl)
+    //     .then(function(response) {
+    //         return response.json();
+    //     }
+    //     .then (function (data){
+    //         displayGenre();
             
-        })
-    )
-    })
+    //     })
+    // )
+    // })
 
-function displayGenre() {
+// function displayGenre() {
     // pull from api text and images to display in html
-    
-}
-
-
 // local storage and search history function
 // var userInputArr=[];
 // var searchHis=localStorage.setItem(userInput)
@@ -50,5 +59,19 @@ function displayGenre() {
 // }
 
 // console.log(userInputArr);
-
-});
+// fetch(newURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data.results)
+//     });
+// localStorage.setItem("cityName", searchTerm.val());
+// // localStorage.getItem(searchTerm.val()); this also pulls it
+// // console.log(localStorage.getItem("cityName"));
+// var cityName = localStorage.getItem("cityName");
+// console.log(cityName);
+// var historyContainer = $(".historyContainer");
+// var searchHistory = $('<button>');
+// searchHistory.text(cityName);
+// historyContainer.append(searchHistory);
