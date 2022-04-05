44193cd30ed9bc1c266dae76ba3d062b73632951
Hannah Lodge <lodg0021@umn.edu>
2022-04-05 13:58:49 -0500

    modified local storage function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,54 +1,65 @@
+// part of initializing the jquery 
 $(document).ready(function(){
-var urlData = document.location.search;
-// var userInput = $('#userInput').val();
-var genreSearch; //= url split
 
-var apiUrl; //= url + genreSearch/genreId + albums
+    var urlData = document.location.search;
+    var userInput = $('#userInput').val();
+    var genreSearch; //= url split
 
-// initializes the form
+    var apiUrl; //= url + genreSearch/genreId + albums
+
+    // initializes the form
     $('select').formSelect();
 
-// var instance = M.FormSelect.getInstance(elem);
 
+    // when search is added on results html
+    var searchBtn = $('#search-btn');
 
-// when search is added on results html
-var searchBtn = $('#search-btn');
+    searchBtn.on("click", function(e) {
+        e.preventDefault();
+        // var artist = ;
+        // var album = ;
 
-searchBtn.on("click", function(e) {
-    e.preventDefault();
-    //var artist = ;
-    //var album = ;
-
-    fetch(apiUrl)
-        .then(function(response) {
-            return response.json();
-        }
-        .then (function (data){
-            displayGenre();
-            
+        fetch(apiUrl)
+            .then(function(response) {
+                // return response.json();
+            }
+            .then (function (data){
+                displayGenre();
+                
+            })
+        )
         })
-    )
-    })
 
-function displayGenre() {
-    // pull from api text and images to display in html
-    
-}
+    function displayGenre() {
+        // pull from api text and images to display in html
+        
+    }
+
+    // local storage function
+    var genreArr=[];
+    var storage=localStorage.getItem("genreName");
+
+    if(userInput){
+        storage.push(userInput);
+        localStorage.setItem("genreName", userInput);
+    } else{
+        genreArr.push(userInput);
+        localStorage.setItem("genreName", userInput);
+    }
+
+    console.log (genreArr);
 
 
-// local storage and search history function
-// var userInputArr=[];
-// var searchHis=localStorage.setItem(userInput)
+    var scoreList=JSON.parse(localStorage.getItem("genreName"));
 
-// if(userInput){
-//     userCity.push(userInputArr);
-//     localStorage.setItem("cities", JSON.stringify(userCity));
-// } 
-// else{
-//     userInputArr.push(userCity);
-//     localStorage.setItem("cities", JSON.stringify(userInputArr));
-// }
+    for(var i=0; i<scoreList.length; i++){
+        var listItem= $.create("li");
+        var list=$("#searchHis");
+        list.append(listItem);
 
-// console.log(userInputArr);
+        listItem.textContent=scoreList[i].initials + "  -  " +scoreList[i].score;
+    };
+
+
 
 });
