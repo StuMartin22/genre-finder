9aea3209b63dbaacded807f2a8bd56a1f48c28a8
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-04-06 10:31:42 -0500

    file restructer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,61 +1,93 @@
 $(document).ready(function(){
-var urlData = document.location.search;
-// var userInput = $('#userInput').val();
-var genreSearch; //= url split
-
-var apiUrl; //= url + genreSearch/genreId + albums
-
-
-// initializes the form
-
-     $('select').formSelect();
-
-
-//var instance = M.FormSelect.getInstance(elem);
-
-
-var timeDisplayE1 = $('#time-display');
-// handle displaying the  time
-    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplayE1.text(rightNow);
-
-
-// when search is added on results html
-//var searchBtn = $('#search-btn');
-
- //searchBtn.on("click", function(e) {
-    //e.preventDefault();
-    //var artist = ;
-    //var album = ;
-
-   // fetch(apiUrl)
-       // .then(function(response) {
-          //  return response.json();
-      //  }
-       // .then (function (data){
-         //   displayGenre();
+    var urlData = document.location.search;
+    // var userInput = $('#userInput').val();
+    var genreSearch; //= url split
+    
+    // part of initializing the jquery 
+        var urlData = document.location.search;
+        var genreSearch; //= url split
+    
+    // initializes the form
+         $('select').formSelect();
+    
+    
+    // var timeDisplayE1 = $('#time-display');
+    // handle displaying the  time
+        // var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+        // timeDisplayE1.text(rightNow);
+    
+        
+        var apiUrl = "https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&limit=10&api_key=38f325c730f4218c2247c79ff7fd0a85&format=json";
+        
+        
+        // var instance = M.FormSelect.getInstance(elem);
+        
+        
+        // when search is added on results html
+        var searchBtn = $('#search-btn');
+        
+        searchBtn.on("click", function(e) {
+            e.preventDefault();
+            var userInput = $('#userInput').val();
+    
+        //     var newURL = apiUrl.split("tag=rock").join(userInput.val());
+        //     console.log(newURL);
+        
+        //     fetch(apiUrl)
+        //         .then(function(response) {
+        //             return response.json();
+        //         }
+        //         .then (function (data){
+        //             displayGenre();
+        
+                    // for (var i = 0; i < data.albums; i++) {
+                    //     // var albumCardEl = $();
+                    //     // var albumNameEl = $();
+                    //     // var artistNameEl = $();
+                    //     // var albumCoverEl = $();
+        
+        
+                    //     // albumNameEl.text(data.albums.album[i].name);
+                    //     // albumCardEl.append(albumNameEl);
+        
+                    //     // artistNameEl.text(data.albums.album[i].artist.name);
+                    //     // albumCardEl.append(artistNameEl);
+        
+                    //     // in the documentation it shows a '#' before text, but this spits an error
+                    //     // albumCoverEl.text(data.albums.album[i].image[1].#text);
+                    //     // albumCardEl.append($('<img>', {id:'albumCover', src: data.albums.album[i].image[1].#text}));
+                    // }
+        
+            //     })
+    
+            // local storage functions, to store search history
+            var genreArr=[];
+            var storage=JSON.parse(localStorage.getItem("genreName"));
+            if(storage){
+                storage.push(userInput);
+                localStorage.setItem("genreName", JSON.stringify(storage));
+            } else{
+                genreArr.push(userInput);
+                localStorage.setItem("genreName", JSON.stringify(genreArr));
+            }
             
-      //  })
-   // )
-  //  })
-
-//function displayGenre() {
-    // pull from api text and images to display in html
-
-
-
- // local storage and search history function
-
-
-
-// if(userInput){
-//     userCity.push(userInputArr);
-//     localStorage.setItem("cities", JSON.stringify(userCity));
-// } 
-// else{
-//     userInputArr.push(userCity);
-//     localStorage.setItem("cities", JSON.stringify(userInputArr));
-// }
-
-// console.log(userInputArr);
-});
+            // puts as list under search history
+            var listGen=JSON.parse(localStorage.getItem("genreName"));
+            for(var i=0; i<listGen.length; i++){
+                var listItem= $('<li>');
+                var list=$("#searchHis");
+                list.append(listItem);
+                listItem.text(listGen[i]);
+            };
+            
+            // )
+            // })
+    
+            // returnBtn.on("click", function(e){
+            //     e.preventDefault();
+            })
+    
+    
+       
+    });
+    
