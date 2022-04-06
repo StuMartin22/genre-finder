f0dffb2c6a788f059b93059d5125818824e3fa46
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-04-06 11:41:25 -0500

    code restructer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,11 +11,6 @@ $(document).ready(function(){
     var timeDisplayE1 = $('#time-display');
     var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
     timeDisplayE1.text(rightNow);
-
-    var timeDisplayE1 = $('#time-display');
-    // handle displaying the  time
-    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplayE1.text(rightNow);
     
     var apiUrl = "https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&limit=10&api_key=38f325c730f4218c2247c79ff7fd0a85&format=json";
     var artistUrl = "https://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=rock&limit=10&api_key=38f325c730f4218c2247c79ff7fd0a85&format=json";
