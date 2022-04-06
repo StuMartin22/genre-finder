375f75a69a3277d411057b9e4bdb1130616c5641
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-06 12:08:29 -0500

    merge

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,8 @@
 $(document).ready(function(){
     var urlData = document.location.search;
-    var userInput = $('.userInput');
-    var urlData = document.location.search;
-    var genreSearch; //= url split
 
     // initializes the form
     $('select').formSelect();
-
-    // handle displaying the  time
-    var timeDisplayE1 = $('#time-display');
-    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplayE1.text(rightNow);
 
     var timeDisplayE1 = $('#time-display');
     // handle displaying the  time
@@ -26,65 +18,66 @@ $(document).ready(function(){
     searchBtn.on("click", function(e) {
         e.preventDefault();
         
-        var userInput = $('#userInput').val();
+        var userInput = $('.userInput').val();
+        console.log(userInput);
 
         var newURL = apiUrl.split("tag=rock").join(userInput.val());
         console.log(newURL);
 
         var secondUrl = artistUrl.split("tag=rock").join(userInput.val());  
         
-        fetch(apiUrl)
-            .then(function(response) {
-                return response.json();
-            })
-            .then (function (data){
-                displayGenre();
+        // fetch(apiUrl)
+        //     .then(function(response) {
+        //         return response.json();
+        //     })
+        //     .then (function (data){
+        //         displayGenre();
     
-                for (var i = 0; i < data.albums; i++) {
-                    var albumCardEl = $();
-                    var albumNameEl = $();
-                    var artistNameEl = $();
-                    var albumCoverEl = $();
+        //         for (var i = 0; i < data.albums; i++) {
+        //             var albumCardEl = $();
+        //             var albumNameEl = $();
+        //             var artistNameEl = $();
+        //             var albumCoverEl = $();
     
     
-                    albumNameEl.text(data.albums.album[i].name);
-                    albumCardEl.append(albumNameEl);
+        //             albumNameEl.text(data.albums.album[i].name);
+        //             albumCardEl.append(albumNameEl);
     
-                    artistNameEl.text(data.albums.album[i].artist.name);
-                    albumCardEl.append(artistNameEl);
+        //             artistNameEl.text(data.albums.album[i].artist.name);
+        //             albumCardEl.append(artistNameEl);
     
-                    // in the documentation it shows a '#' before text, but this spits an error
-                    // albumCoverEl.text(data.albums.album[i].image[1].#text);
-                    // albumCardEl.append($('<img>', {id:'albumCover', src: data.albums.album[i].image[1].#text}));
-                };
+        //             // in the documentation it shows a '#' before text, but this spits an error
+        //             // albumCoverEl.text(data.albums.album[i].image[1].#text);
+        //             // albumCardEl.append($('<img>', {id:'albumCover', src: data.albums.album[i].image[1].#text}));
+        //         };
     
         // local storage functions, to store search history
-        var genreArr=[];
-        var storage=JSON.parse(localStorage.getItem("genreName"));
-        if(storage){
-            storage.push(userInput);
-            localStorage.setItem("genreName", JSON.stringify(storage));
-        } else{
-            genreArr.push(userInput);
-            localStorage.setItem("genreName", JSON.stringify(genreArr));
-        }
+    //     var genreArr=[];
+    //     var storage=JSON.parse(localStorage.getItem("genreName"));
+    //     if(storage){
+    //         storage.push(userInput);
+    //         localStorage.setItem("genreName", JSON.stringify(storage));
+    //     } else{
+    //         genreArr.push(userInput);
+    //         localStorage.setItem("genreName", JSON.stringify(genreArr));
+    //     }
         
-        // puts as list under search history
-        var listGen=JSON.parse(localStorage.getItem("genreName"));
-        console.log (listGen)
-        for(var i=0; i<listGen.length; i++){
-            var listItem= $('<li>');
-            var list=$("#searchHis");
-            list.append(listItem);
-            listItem.text(listGen[i]);
-        };  
-    });
+    //     // puts as list under search history
+    //     var listGen=JSON.parse(localStorage.getItem("genreName"));
+    //     console.log (listGen)
+    //     for(var i=0; i<listGen.length; i++){
+    //         var listItem= $('<li>');
+    //         var list=$("#searchHis");
+    //         list.append(listItem);
+    //         listItem.text(listGen[i]);
+    //     };  
+    // });
 
         
-        });                                                                    
+    //     });                                                                    
 
     //     returnBtn.on("click", function(e){
     //         e.preventDefault();
     // });
 
-});
+})});
